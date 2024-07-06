24ea41d4ffc1437c38c64221a987075eb23327f8
OwN41 <p.w.2004@gmx.de>
2024-07-06 15:14:20 +0200

    .

--- src/helpers/registryHandler.js
+++ src/helpers/registryHandler.js
@@ -1,15 +1,18 @@
+
 export default class RegistryHandler{
     constructor(){}    
 
     readRegistry() {
+        let temp  = [];
         fetch("http://localhost:5173/src/helpers/registry.json")
         .then((res) => res.json())
         .then((json) => {
+            temp = json
             return json;
         });    
     }
 
     writeRegistry() {
-
+        
     }
 }
