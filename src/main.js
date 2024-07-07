12f3f75d719a9a5da185bffa17ac7ccfce8c68c6
Glenn Strommer <glenn@strommeronline.de>
2024-07-07 11:03:43 +0200

    e

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import Login from './components/login.vue'
-import Register from './components/register.vue'
 import Dashboard from './components/dashboard.vue'
 
 const app = createApp(App)
 
 app.component('login', Login)
-app.component('register', Register)
 app.component('dashboard', Dashboard)
 
 app.mount('#app')
